ce5816fcce379fa12790bc83f15c317a6b0dd868
Final Calibur <fred11212@hotmail.com>
2016-03-06 22:26:32 -0500

    Priority enum & Comparators class pretty much finished

--- src/task/Comparators.java
+++ src/task/Comparators.java
@@ -1,6 +1,55 @@
 package task;
 
+import java.util.Comparator;
+
+/**
+ * This class contains the various Comparator objects
+ * that the system will use to compare tasks.
+ * 
+ * This is an easy and efficient way to sort tasks. With
+ * these comparators, we will be able to use Collections.sort
+ * to sort collections of Tasks quickly and conveniently.
+ * 
+ * @author Freddy
+ */
+
 public class Comparators
 {
-
+	//constants to avoid instantiating the comparator every time
+	public static final Comparator<Task> CREATION_DATE = creationDateComparator();
+	public static final Comparator<Task> DUE_DATE = dueDateComparator();
+	public static final Comparator<Task> NAME = nameComparator();
+	public static final Comparator<Task> CATEGORY = categoryComparator();
+	public static final Comparator<Task> DURATION = durationComparator();
+	public static final Comparator<Task> PRIORITY = priorityComparator();
+	
+	private static Comparator<Task> creationDateComparator()
+	{
+		return(Task one, Task two)->one.getCreationDate().compareTo(two.getCreationDate());
+	}
+	
+	private static Comparator<Task> dueDateComparator()
+	{
+		return(Task one, Task two)->one.getDueDate().compareTo(two.getDueDate());
+	}
+	
+	private static Comparator<Task> nameComparator()
+	{
+		return(Task one, Task two)->one.getTaskName().compareTo(two.getTaskName());
+	}
+	
+	private static Comparator<Task> categoryComparator()
+	{
+		return(Task one, Task two)->one.getCategory().compareTo(two.getCategory());
+	}
+	
+	private static Comparator<Task> durationComparator()
+	{
+		return(Task one, Task two)->one.getDuration().compareTo(two.getDuration());
+	}
+	
+	private static Comparator<Task> priorityComparator()
+	{
+		return(Task one, Task two)->one.getPriority().compareTo(two.getPriority());
+	}
 }
--- src/task/Task.java
+++ src/task/Task.java
@@ -1,6 +1,68 @@
 package task;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.Stack;
+
 public class Task
 {
-
+	private LocalDateTime creationDate;
+	private LocalDateTime dueDate;
+	private Duration duration;
+	private String taskName;
+	private String taskDescription;
+	private String category;
+	private Priority priority;
+	private Stack<Comment> comments;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/* Getters & Setters */
+	
+	public LocalDateTime getCreationDate()
+	{
+		return creationDate;
+	}
+	
+	public LocalDateTime getDueDate()
+	{
+		return dueDate;
+	}
+	
+	public String getTaskName()
+	{
+		return taskName;
+	}
+	
+	public String getTaskDescription()
+	{
+		return taskDescription;
+	}
+	
+	public String getCategory()
+	{
+		return category;
+	}
+	
+	public Priority getPriority()
+	{
+		return priority;
+	}
+	
+	public Duration getDuration()
+	{
+		return duration;
+	}
+	
+	public Stack<Comment> getComments()
+	{
+		return comments;
+	}
 }
