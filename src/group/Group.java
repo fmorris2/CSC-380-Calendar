d70c142ccba047e161dcd10d1f914277335f3478
NP <npierce@oswego.edu>
2016-03-10 13:51:31 -0500

    Final commit (group, groupTest, and permissions) before submission - Noah

--- src/group/Group.java
+++ src/group/Group.java
@@ -1,6 +1,172 @@
 package group;
 
-public class Group
-{
+import java.util.ArrayList;
+import java.util.HashMap;
 
+import task.Task;
+import user.Permissions;
+import user.User;
+
+public class Group {
+   
+	//Variables
+    
+	String groupName;
+    String groupDescription;
+    ArrayList<Task> groupTasks;
+    ArrayList<User> groupMembers;
+    User groupLeader;
+    HashMap<User, Permissions> permissions;
+    
+    
+    //Constructors
+    
+    /**
+     * The parameterized constructor for the group class.
+     * @param leader
+     * @param name
+     * @param description 
+     */
+    public Group(User leader, String name, String description){
+        this.groupDescription = description;
+        this.groupName = name;
+        this.groupLeader = leader;
+        this.groupMembers = new ArrayList<>();
+        this.permissions = new HashMap<User, Permissions>();
+        addUser(this.groupLeader, true, true, true);
+        this.groupTasks = new ArrayList<Task>();
+        
+    }
+  
+    /**
+    * A default constructor for testing the group class.
+    */
+    public Group(){
+        this.groupDescription = "Group Description";
+        this.groupName = "Test Group";
+        this.groupTasks = new ArrayList<Task>();
+        this.groupMembers = new ArrayList<User>();
+        Task a = new Task(); Task b = new Task();
+        User gm = new User();
+        this.groupLeader = gm;
+        this.permissions = new HashMap<User, Permissions>();
+        addUser(this.groupLeader, true, true, true);
+        addGroupTask(this.groupLeader, a); addGroupTask(this.groupLeader, b);
+        
+        
+
+        
+        
+    }
+    
+    
+    //Adding new tasks to the group
+    
+    /**
+     * Adds a new task to the group task list
+     * @param task
+     */
+    public void addGroupTask(User user, Task task) {
+		//Need to assign task to a user
+    	if(permissions.get(user).getCanCreateTasks()){
+    		this.groupTasks.add(task);
+//    		for (User i : this.groupMembers){
+//    			if (this.permissions.get(i).getCanCompleteTasks()){
+//    				i.addNewTask(task);
+//    			}
+//    		}
+    	}
+    	
+	}
+    
+    //Removing task from the group
+    
+    /**
+     * Removes a from the group task list
+     * @param task
+     */
+    public void removeGroupTask(Task task) {
+    	//Check permissions of user
+    	//Interact with tasks of user
+    	this.groupTasks.remove(task);
+    }
+
+	//Adding Users to Group
+    
+    
+    /**
+     * Method adds a new user to the group.
+     * @param user
+     * @param change
+     * @param create
+     * @param complete 
+     */
+    public void addUser(User user, Boolean change, Boolean create, Boolean complete) {
+        //Check if user is a group leader
+    	this.groupMembers.add(user);
+        addPermissions(user, change, create, complete);
+        user.addGroup(this);
+    }
+    
+   
+    private void addPermissions(User user, Boolean change, Boolean create, Boolean complete) {
+    	Permissions newPerm = new Permissions(change, create, complete);
+    	permissions.put(user, newPerm);
+		
+	}
+
+	//Removing user from the group
+    public void removeUser(User user) {
+    	//Check for group member
+    	this.groupMembers.remove(user);
+    	this.permissions.remove(user);
+    	user.removeGroup(this);
+    	
+    }
+    
+    //Changing Permissions
+    
+    
+    /**
+     * Changes permissions, and returns a new copy of the permissions list with changes.
+     * @param user
+     * @param change
+     * @param create
+     * @param complete
+     * @return
+     */
+    public void setPermissions(User user, boolean change, boolean create, boolean complete) {
+    	//Needs to check whether person is a group leader
+    	
+//    	if (this.permissions.containsKey(user)){
+    	permissions.get(user).setCanChangeTasks(change);
+        permissions.get(user).setCanCompleteTasks(complete);
+        permissions.get(user).setCanCreateTasks(create);
+//        } else {
+        	
+//        }
+    }
+    
+    
+    
+    
+    
+    
+    //Other getters and setters
+    
+    
+    
+    public String getGroupName(){ return groupName;}
+    public void setGroupName(String groupName) { groupName = this.groupName;}
+    public String getGroupDescription(){ return groupDescription;}
+    public void setGroupDescription(String groupDescription){ groupDescription = this.groupDescription;}
+    public ArrayList<Task> getGroupTasks() {return groupTasks;}
+    public void setGroupTasks(ArrayList<Task> groupTasks){groupTasks = this.groupTasks;}
+    public ArrayList<User> getGroupMembers() {return groupMembers;}
+    public void setGroupMembers(ArrayList<User> groupMembers){groupMembers = this.groupMembers;}
+    public User getGroupLeader(){return groupLeader;}
+    public void setGroupLeader(User groupLeader){groupLeader = this.groupLeader;}
+    public HashMap<User, Permissions> getUserPermissions(){return permissions;}
+    public void setUserPermissions(HashMap<User, Permissions> permissions){ permissions = this.permissions;}
+    
 }
--- src/tests/GroupTest.java
+++ src/tests/GroupTest.java
@@ -1,24 +1,73 @@
 package tests;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
-
-import java.time.LocalDateTime;
 
 import org.junit.Test;
 
-import filters.Filter;
-import filters.Order;
-import filters.date.impl.DueDateFilter;
-import filters.impl.CategoryFilter;
+import group.Group;
+import task.Task;
+import user.Permissions;
+import user.User;
+
 
 public class GroupTest {
 
 	@Test
 	public void test() {
-		Filter f = new CategoryFilter("homework").and(
-				new DueDateFilter(LocalDateTime.now(), Order.EQUAL, Order.BEFORE));
+
 		
-		fail("Not yet implemented");
+		//Create test group
+		Group test = new Group();
+		
+		//Assert that test group was created properly
+		if(!"Test Group".equals(test.getGroupName()))
+			fail("Test Group failed creation (Group())");
+		
+		//Check if new members can be added to the group
+		User userTest = new User("Bob", "Greg", "Username", "Password", "Email");
+		test.addUser(userTest, true, true, true);
+		if(!test.getGroupMembers().contains(userTest))
+			fail("User was not added to group list (addUser)");
+		if(!test.getUserPermissions().containsKey(userTest))
+			fail("New user was not added to permissions list (addUser)");
+		
+		//Check the permissions of the newly added user
+		assertTrue("The permissions of a new user did not match up (setPermissions)", test.getUserPermissions().get(userTest).getCanChangeTasks());
+		assertTrue("The permissions of a new user did not match up (setPermissions)", test.getUserPermissions().get(userTest).getCanCompleteTasks());
+		assertTrue("The permissions of a new user did not match up (setPermissions)", test.getUserPermissions().get(userTest).getCanCreateTasks());
+		
+		//Add a new task
+		Task task = new Task();
+		test.addGroupTask(userTest, task);
+		if (!test.getGroupTasks().contains(task))
+			fail("Failed to add task (addGroupTask");
+		
+		//Change permissions of a user
+		Permissions change = new Permissions(true, false, true);
+		test.setPermissions(userTest, true, false, true);
+		if(test.getUserPermissions().get(userTest).equals(change))
+			fail("Failed to set permissions of a users (setPermissions)");
+		
+		//Remove user from group
+		test.removeUser(userTest);
+		
+		//Check to make sure user was taken out of group
+		if(test.getGroupMembers().contains(userTest))
+			fail("User was not removed (removeUser)");
+		
+		//Check to make sure user has lost their permissions
+		if(test.getUserPermissions().containsKey(userTest))
+			fail("Failed to revoke user permisssions (removeUser)");
+		
+		
+		
+		//Remove a task
+		test.removeGroupTask(task);
+		if (test.getGroupTasks().contains(task))
+			fail ("Task remove failed (removeGroupTask");
+		
+		
 	}
 
 }
