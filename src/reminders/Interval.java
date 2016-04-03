63bdbe58d87d00859c55982d33721c6365da61ba
NP <npierce@oswego.edu>
2016-04-02 22:51:43 -0400

    Changes to reminder class

---
+++ src/reminders/Interval.java
@@ -0,0 +1,17 @@
+package reminders;
+
+
+public enum Interval {
+	ONE_HOUR(60), TWO_HOURS(120), ONE_WEEK(10080), TWO_WEEKS(20160), HALF_HOUR(30), FIFTEEN_MINUTES(15), TEN_MINUTES(10), FIVE_MINUTES(5); 
+	
+	private int interval;
+	
+	Interval(int interval){
+		this.interval = interval;
+	}
+
+	public int getInterval() {
+		return interval;
+	}
+
+}
---
+++ src/reminders/Reminder.java
@@ -0,0 +1,50 @@
+package reminders;
+
+import java.time.LocalDateTime;
+
+public class Reminder {
+	public LocalDateTime dueDate;
+	public boolean remind;
+	public boolean reminderSent;
+	
+	public Reminder(LocalDateTime dueDate, Interval interval){
+		this.dueDate = dueDate.minusMinutes(interval.getInterval());
+		reminderSent = false;
+	}
+	
+	public boolean remindTime() {
+		LocalDateTime now = LocalDateTime.now();
+		if (reminderSent == false) {
+			if (dueDate.isBefore(now)) {
+				reminderSent = true;
+				return dueDate.isBefore(now);
+			} 
+		}
+		return false;
+	}
+
+	public LocalDateTime getDueDate() {
+		return dueDate;
+	}
+
+	public void setDueDate(LocalDateTime dueDate) {
+		this.dueDate = dueDate;
+	}
+
+	public boolean isRemind() {
+		return remind;
+	}
+
+	public void setRemind(boolean remind) {
+		this.remind = remind;
+	}
+
+	public boolean isReminderSent() {
+		return reminderSent;
+	}
+
+	public void setReminderSent(boolean reminderSent) {
+		this.reminderSent = reminderSent;
+	}
+	
+}
