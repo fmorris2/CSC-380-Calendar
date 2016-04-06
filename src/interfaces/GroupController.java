cc8ed7ec7ecae97db85519346352518bc1857cd9
NP <npierce@oswego.edu>
2016-04-06 18:40:28 -0400

    always save your work before commiting

--- src/interfaces/GroupController.java
+++ src/interfaces/GroupController.java
@@ -3,21 +3,24 @@ package interfaces;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import group.Group;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import user.User;
 
 public class GroupController implements Initializable
 {
-	@FXML groupNameFieldGroup;
-	@FXML groupDescriptionFieldGroup;
+	@FXML TextField groupNameFieldGroup;
+	@FXML TextArea groupDescriptionFieldGroup;
 	User user;
 	
 	@FXML
 	private void handleGroupSubmitListener(ActionEvent event)
 	{
-		
+		Group group = new Group(this.user, groupNameFieldGroup.getText(), groupDescriptionFieldGroup.getText());
 	}
 	@Override
 	public void initialize(URL location, ResourceBundle resources)
