6757b3a63e1d17b5e655ddb596bb6475b8d8c1d2
NP <npierce@oswego.edu>
2016-04-21 21:09:42 -0400

    Addition of window to be used to add reminders

---
+++ src/interfaces/AddReminderController.java
@@ -0,0 +1,86 @@
+package interfaces;
+
+import java.io.IOException;
+import java.net.URL;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import reminders.Interval;
+import reminders.Reminder;
+import task.Priority;
+import task.Task;
+import user.User;
+
+public class AddReminderController {
+	
+	@FXML
+	DatePicker datePickerReminders;
+	@FXML
+	ChoiceBox<Interval> intervalPickerReminder;
+	@FXML
+	Label SystemMessage;
+	User user;
+	Task currentTask;
+	MainController parent;
+	
+	
+	@FXML
+	public void onEnter(ActionEvent event)
+	{
+		try
+		{
+			handleAddReminderListener(event);
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	@FXML
+	private void handleAddReminderListener(ActionEvent event) throws IOException
+	{
+		if(validFields())
+		{
+			currentTask.addReminder(intervalPickerReminder.getValue());
+			parent.refreshList();
+			Stage stage = (Stage) intervalPickerReminder.getScene().getWindow();
+			stage.close();
+		}
+		else
+		{
+			SystemMessage.setText("One or more fields are incorrectly filled out.");
+		}
+	}
+
+	private boolean validFields() {
+		if (!intervalPickerReminder.getValue().toString().equals("")){
+			return true;
+		}
+		return false;
+	}
+	
+	public void initialize(URL location, ResourceBundle resources)
+	{
+		this.user = InterfaceLauncher.CurrentUser;
+		
+	}
+	
+	public void setParent(MainController p)
+	{
+		parent = p;
+	}
+	
+	public void setTask(Task t)
+	{
+		currentTask = t;
+	}
+	
+}
